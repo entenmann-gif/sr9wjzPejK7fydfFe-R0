11b6990cce2ce1981ebbf55c1406bac68f6f22a2
stolpersteinebs <merlin.kreuzer@cjd-bs.de>
2026-03-20 18:40:06 +0100

    Add Supabase configuration with URL and anon key

---
+++ supabase-config.js
@@ -0,0 +1,2 @@
+window.SUPABASE_URL = 'https://mvyglrnpmhkfhwhuwpin.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12eWdscm5wbWhrZmh3aHV3cGluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMDk4ODgsImV4cCI6MjA4OTU4NTg4OH0.1weQjAHOKtW1SMBtvZ48EeZda8GRzEETZNXPv_bw1h0';
